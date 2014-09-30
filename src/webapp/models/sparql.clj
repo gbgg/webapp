4b3931e25f1d26c825fe6e68a255736630ae2b83
Gene Gragg <g-gragg@uchicago.edu>
2014-09-30 15:57:23 -0500

    now have fn that generate standard sparql and matsu-format queries from input form

--- src/webapp/models/sparql.clj
+++ src/webapp/models/sparql.clj
@@ -13,59 +13,14 @@
   (:use [hiccup.page :only [html5]])
 )
 
-;;(defn query-form [language lpref valstring]
-;;  (let [values (split valstring #",")
-;;        valsection (doseq [value values]
-;;            (str ":s :Q" value " ["lpref" "value"] \\.")
-;;            (str ":Q" value " [:rdfs :label] "value" \\."))]
-;;    (pdgm-qry language lpref valstring)))
+;; some common prefixes
+(register-namespaces {:rdfs "<http://www.w3.org/2000/01/rdf-schema#>"
+                      :aama "<http://id.oi.uchicago.edu/aama/2013/>"
+                      :aamas "<http://id.oi.uchicago.edu/aama/2013/schema/>"
+                      :aamag "<http://oi.uchicago.edu/aama/2013/graph/>"
+                      :bar "<http://id.oi.uchicago.edu/aama/2013/beja-arteiga/>"})
 
-(defn pdgmquery [language lang valstring]
-    ;;(str language " : " valstring)
-    (let [values (split valstring #",")
-          Language (capitalize language)
-          ]
-      ;;(println
-      (tmpl/render-string 
-       (str
-        "(select :lex :num :pers :gen :token)
-        (where (graph [:aamag :{{language}}]
-        :s [:{{lang}} :pos] [:{{lang}} :Verb]  \\.
-	:s [:aamas :lang] [:aama :{{Language}}]  \\.
-	:s [:aamas :lang] :lang  \\.
-	:lang [:rdfs :label] :langLabel  \\.")
-       {:lang lang
-        :language language
-        :Language Language})
-      ;;)
-        (doseq [value values]
-        ;;  (println
-        (tmpl/render-string 
-         (str
-          ":s :Q{{value}} [:{{lang}} :{{value}}] \\.
-          :Q{{value}} [:rdfs :label] :{{value}}  \\.")
-         {:value value
-          :lang lang}))
-        ;;)
-        ;;(println
-      (tmpl/render-string
-       (str
-	"(optional :s [:aamas :lexeme] :lex)  \\. 
-	(optional :s [:{{lang}} :number] :number)  \\.
-	(optional :number [:rdfs :label] :num)  \\. 
-	(union (group :s [:{{lang}} :pngShapeClass] :person)  
-	       (group :s [:{{lang}} :person] :person))  \\.
-	:person [:rdfs :label] :pers  \\.
-	(optional :s [:{{lang}} :gender] :gender)  \\.
-	(optional :gender [:rdfs :label] :gen)  \\. 
-	:s [:{{lang}} :token] :token  \\.))
-        (order-by :lex (desc :num) :pers (desc :gen)))")
-       {:lang lang})
-      ;;)
-))
-
-
-;; matsu version
+;;canned query matsu-format. This works
 (defquery aama-qry []
 (select :lex :num :pers :gen :token)
 (where (graph [:aamag :beja-arteiga]
@@ -90,6 +45,231 @@
 	(optional :s [:bar :gender] :gender)  \.
 	(optional :gender [:rdfs :label] :gen)  \. 
 	:s [:bar :token] :token  \.))
-(order-by :lex (desc :num) :pers (desc :gen)))
+         (order-by :lex (desc :num) :pers (desc :gen)))
+
+;;attempt to generate query in matsu-format using stencil/render-string. 
+;;If println uncommented, prints correct query as side-effect,
+;;But:
+;; 1) will not return doseq section w/o println
+;; 2) even if doseq worked, don't know how to turn this into 
+;;    a working "(pdgm-qry)"
+(defn pdgmqry-matsu [language lpref valstring]
+    (let [values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+       ;;(println
+      (tmpl/render-string 
+       (str
+        ;;"(defquery pdgm-qry []
+        "(select :lex :num :pers :gen :token)
+        (where (graph [:aamag :{{language}}]
+        :s [:{{lpref}} :pos] [:{{lpref}} :Verb]  \\.
+	:s [:aamas :lang] [:aama :{{Language}}]  \\.
+	:s [:aamas :lang] :lang  \\.
+	:lang [:rdfs :label] :langLabel  \\.")
+       {:lang lang
+        :language language
+        :Language Language})
+       ;;);;println
+      (doseq [value values]
+       ;;(println
+        (tmpl/render-string 
+         (str
+          ":s :Q{{value}} [:{{lpref}} :{{value}}] \\.
+          :Q{{value}} [:rdfs :label] :{{value}}  \\.")
+         {:value value
+          :lang lang})
+        ;;);;println
+       )
+       (println
+      (tmpl/render-string
+       (str
+	"(optional :s [:aamas :lexeme] :lex)  \\. 
+	(optional :s [:{{lpref}} :number] :number)  \\.
+	(optional :number [:rdfs :label] :num)  \\. 
+	(union (group :s [:{{lpref}} :pngShapeClass] :person)  
+	       (group :s [:{{lpref}} :person] :person))  \\.
+	:person [:rdfs :label] :pers  \\.
+	(optional :s [:{{lpref}} :gender] :gender)  \\.
+	(optional :gender [:rdfs :label] :gen)  \\. 
+	:s [:{{lpref}} :token] :token  \\.))
+        (order-by :lex (desc :num) :pers (desc :gen)))")
+       {:lang lang})
+       );;println
+      )
+))
+
+(defn pdgmquery [language lang valstring]
+    (let [values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+      ;;(println
+      (tmpl/render-string 
+       (str
+        ;;"(defquery pdgm-qry []
+        "(select :lex :num :pers :gen :token)
+        (where (graph [:aamag :{{language}}]
+        :s [:{{lang}} :pos] [:{{lang}} :Verb]  \\.
+	:s [:aamas :lang] [:aama :{{Language}}]  \\.
+	:s [:aamas :lang] :lang  \\.
+	:lang [:rdfs :label] :langLabel  \\.")
+       {:lang lang
+        :language language
+        :Language Language})
+        ;;)
+        (doseq [value values]
+       ;;(println
+        (tmpl/render-string 
+         (str
+          ":s :Q{{value}} [:{{lang}} :{{value}}] \\.
+          :Q{{value}} [:rdfs :label] :{{value}}  \\.")
+         {:value value
+          :lang lang}))
+         ;;)
+       ;;(println
+      (tmpl/render-string
+       (str
+	"(optional :s [:aamas :lexeme] :lex)  \\. 
+	(optional :s [:{{lang}} :number] :number)  \\.
+	(optional :number [:rdfs :label] :num)  \\. 
+	(union (group :s [:{{lang}} :pngShapeClass] :person)  
+	       (group :s [:{{lang}} :person] :person))  \\.
+	:person [:rdfs :label] :pers  \\.
+	(optional :s [:{{lang}} :gender] :gender)  \\.
+	(optional :gender [:rdfs :label] :gen)  \\. 
+	:s [:{{lang}} :token] :token  \\.))
+        (order-by :lex (desc :num) :pers (desc :gen))))")
+       {:lang lang})
+        ;;)
+      )
+))
 
 
+;; Breaking up pdgmqry-sparql into pdgmgrq-sparqlABC
+;; is inelegant, but works
+(defn pdgmqry-sparqlA [language lpref]
+    (let [;;values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+       ;;(println
+      (tmpl/render-string 
+       (str
+	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+	PREFIX aama: <http://id.oi.uchicago.edu/aama/2013/> 
+	PREFIX aamas: <http://id.oi.uchicago.edu/aama/2013/schema/> 
+	PREFIX aamag: <http://oi.uchicago.edu/aama/2013/graph/> 
+	PREFIX {{lpref}}: <http://id.oi.uchicago.edu/aama/2013/{{language}}/> 
+	SELECT ?lex ?num ?pers ?gen ?token  
+	WHERE\n{ 
+	 { 
+	  GRAPH aamag:{{language}}\n  { 
+	   ?s {{lpref}}:pos {{lpref}}:Verb .  
+	   ?s aamas:lang aama:{{Language}} . 
+	   ?s aamas:lang ?lang . 
+	   ?lang rdfs:label ?langLabel .  ")
+       {:lpref lpref
+        :language language
+        :Language Language})
+      ;;);;println
+      );;str
+      )
+)
+
+(defn pdgmqry-sparqlB [lpref  valstring]
+    (let [values (split valstring #",")]
+        (for [value values]
+        ;;(println
+         (str
+          "?s ?Q" value " " lpref ":" value " ."  
+	  "?Q" value " rdfs:label ?" value " .  ")
+         ;;);;println
+         )
+ ))
+
+(defn pdgmqry-sparqlC [lpref]
+  (str
+        (tmpl/render-string
+       (str
+       "   OPTIONAL { ?s aamas:lexeme ?lex . }  
+	   OPTIONAL { ?s {{lpref}}:number ?number .  
+	   ?number rdfs:label ?num . } 
+	   {   ?s {{lpref}}:pngShapeClass ?person .}  
+	   UNION  
+	   {   ?s {{lpref}}:person ?person .}  
+	   ?person rdfs:label ?pers .  
+	   OPTIONAL { ?s {{lpref}}:gender ?gender .  
+	   ?gender rdfs:label ?gen . } 
+	   ?s {{lpref}}:token ?token .  
+	  } 
+	 } 
+	} 
+	ORDER BY ?lex DESC(?num) ?pers DESC(?gen) ")
+       {:lpref lpref})
+        );;str
+)
+
+
+;;generate standard SPARQL query using stencil/render-string. 
+;;This version contains in the "for [value values]" section
+;;the triple "?Q{{value}} rdfs:label ?{{value}} .", which
+;;essentially stipulates that the property to which the value
+;;in question belongs has in fact an rdfs:label. At present
+;;not certain why this seemed necessary at some point.
+(defn pdgmqry-sparql-alt [language lpref valstring]
+    (let [values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+      (tmpl/render-string 
+       (str
+	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+	PREFIX aama: <http://id.oi.uchicago.edu/aama/2013/> 
+	PREFIX aamas: <http://id.oi.uchicago.edu/aama/2013/schema/> 
+	PREFIX aamag:	 <http://oi.uchicago.edu/aama/2013/graph/> 
+	PREFIX {{lpref}}:   <http://id.oi.uchicago.edu/aama/2013/{{language}}/> 
+	SELECT ?lex ?num ?pers ?gen ?token  
+	WHERE\n{ 
+	 { 
+	  GRAPH aamag:{{language}}\n  { 
+	   ?s {{lpref}}:pos {{lpref}}:Verb .  
+	   ?s aamas:lang aama:{{Language}} . 
+	   ?s aamas:lang ?lang . 
+	   ?lang rdfs:label ?langLabel .  ")
+       {:lpref lpref
+        :language language
+        :Language Language})
+      (apply str  
+             (for [value values]
+        (tmpl/render-string 
+         (str
+          "?s ?Q{{value}}  {{lpref}}:{{value}} .  
+	   ?Q{{value}} rdfs:label ?{{value}} .  
+          ")
+         {:value value
+          :lpref lpref})
+          )
+      )
+      (tmpl/render-string
+       (str
+       "   OPTIONAL { ?s aamas:lexeme ?lex . }  
+	   OPTIONAL { ?s {{lpref}}:number ?number .  
+	   ?number rdfs:label ?num . } 
+	   {   ?s {{lpref}}:pngShapeClass ?person .}  
+	   UNION  
+	   {   ?s {{lpref}}:person ?person .}  
+	   ?person rdfs:label ?pers .  
+	   OPTIONAL { ?s {{lpref}}:gender ?gender .  
+	   ?gender rdfs:label ?gen . } 
+	   ?s {{lpref}}:token ?token .  
+	  } 
+	 } 
+	} 
+	ORDER BY ?lex DESC(?num) ?pers DESC(?gen) ")
+       {:lpref lpref})
+       );;str
+))
--- src/webapp/routes/home.clj
+++ src/webapp/routes/home.clj
@@ -6,6 +6,7 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [clojure.string :refer [capitalize split]]
+            [stencil.core :as tmpl]
             [clj-http.client :as http]
             [boutros.matsu.sparql :refer :all]
             [boutros.matsu.core :refer [register-namespaces]]
@@ -22,16 +23,7 @@
                       :aamag "<http://oi.uchicago.edu/aama/2013/graph/>"
                       :bar "<http://id.oi.uchicago.edu/aama/2013/beja-arteiga/>"})
 
-;;(defn show-pdgm [language labbrev valstring]
-;;       (let [pquery (sparql/pdgmquery language labbrev valstring)]
-;;         [:p pquery]))
-
-;;(defn form-pdgm [language labbrev valstring]
-;;       (let [value-section (sparql/valsection valstring)]
-;;         (pdgm-qry language lpref value-section)))
-
-;;(def values (split valstring #","))
-
+;;canned query matsu-format. This works
 (defquery aama-qry []
 (select :lex :num :pers :gen :token)
 (where (graph [:aamag :beja-arteiga]
@@ -56,66 +48,195 @@
 	(optional :s [:bar :gender] :gender)  \.
 	(optional :gender [:rdfs :label] :gen)  \. 
 	:s [:bar :token] :token  \.))
-(order-by :lex (desc :num) :pers (desc :gen)))
+         (order-by :lex (desc :num) :pers (desc :gen)))
 
-(defquery pdgm-qry [language lpref]
-        (select :lex :num :pers :gen :token)
-        (where (graph [:aamag (str ":"language)]
-        :s [lpref :pos] [lpref :Verb]  \.
-	:s [:aamas :lang] [:aama (str ":" (capitalize language))]  \.
-	:s [:aamas :lang] :lang  \.
-	:lang [:rdfs :label] :langLabel  \.
-        ;;valsection
-        (optional :s [:aamas :lexeme] :lex)  \. 
-	(optional :s [lpref :number] :number)  \.
-	(optional :number [:rdfs :label] :num)  \. 
-	(union (group :s [lpref :pngShapeClass] :person)  
-	       (group :s [lpref :person] :person))  \.
-	:person [:rdfs :label] :pers  \.
-	(optional :s [lpref :gender] :gender)  \.
-	(optional :gender [:rdfs :label] :gen)  \. 
-	:s [lpref :token] :token  \.))
-        (order-by :lex (desc :num) :pers (desc :gen)))
+;;attempt to generate query in matsu-format using stencil/render-string. 
+;;Yields correct query form, but not sure how to turn it into working
+;;code.
+;;[NB cf below pdgmqry-sparql on absence of: 
+;;      ":Q{{value}} [:rdfs :label] :{{value}}  \\." 
+;;from "for [value values] section.]
+(defn pdgmqry-matsu [language lpref valstring]
+    (let [values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+      (tmpl/render-string 
+       (str
+        "(defquery pdgm-qry []
+         :lex :num :pers :gen :token)
+        (where (graph [:aamag :{{language}}]
+        :s [:{{lpref}} :pos] [:{{lpref}} :Verb]  \\.
+	:s [:aamas :lang] [:aama :{{Language}}]  \\.
+	:s [:aamas :lang] :lang  \\.
+	:lang [:rdfs :label] :langLabel  \\.")
+       {:lpref lpref
+        :language language
+        :Language Language})
+      (apply str
+      (for [value values]
+        (tmpl/render-string 
+         (str
+          ":s :Q{{value}} [:{{lpref}} :{{value}}] \\.
+          ")
+         {:value value
+          :lpref lpref})
+       )
+      )
+      (tmpl/render-string
+       (str
+	"(optional :s [:aamas :lexeme] :lex)  \\. 
+	(optional :s [:{{lpref}} :number] :number)  \\.
+	(optional :number [:rdfs :label] :num)  \\. 
+	(union (group :s [:{{lpref}} :pngShapeClass] :person)  
+	       (group :s [:{{lpref}} :person] :person))  \\.
+	:person [:rdfs :label] :pers  \\.
+	(optional :s [:{{lpref}} :gender] :gender)  \\.
+	(optional :gender [:rdfs :label] :gen)  \\. 
+	:s [:{{lpref}} :token] :token  \\.))
+        (order-by :lex (desc :num) :pers (desc :gen)))")
+       {:lpref lpref})
+      )
+))
 
-(defn query-form [language lpref valstring]
-  (let [values (split valstring #",")
-        valsection (doseq [value values]
-            (str ":s :Q" value " ["lpref" "value"] \\.")
-            (str ":Q" value " [:rdfs :label] "value"  \\."))]
-    (pdgm-qry language lpref valsection)))
+;; canned query, sparql-format. This works
+(def pdgm-qry 
+"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX aama: <http://id.oi.uchicago.edu/aama/2013/>
+PREFIX aamas: <http://id.oi.uchicago.edu/aama/2013/schema/>
+PREFIX aamag: <http://oi.uchicago.edu/aama/2013/graph/>
+PREFIX bar: <http://id.oi.uchicago.edu/aama/2013/beja-arteiga/>
+SELECT ?lex ?num ?pers ?gen ?token
+WHERE
+{
+{
+GRAPH aamag:beja-arteiga
+{
+?s bar:pos bar:Verb .
+?s aamas:lang aama:Beja-arteiga .
+?s aamas:lang ?lang .
+?lang rdfs:label ?langLabel .
+?s ?QPrefix bar:Prefix.
+?QPrefix rdfs:label ?Prefix .
+?s ?QAffirmative bar:Affirmative.
+?QAffirmative rdfs:label ?Affirmative .
+?s ?QCCY bar:CCY.
+?QCCY rdfs:label ?CCY .
+?s ?QAorist bar:Aorist .
+?QAorist rdfs:label ?Aorist .
+OPTIONAL { ?s aamas:lexeme ?lex . }
+OPTIONAL { ?s bar:number ?number .
+?number rdfs:label ?num . }
+{ ?s bar:pngShapeClass ?person .}
+UNION
+{ ?s bar:person ?person .}
+?person rdfs:label ?pers .
+OPTIONAL { ?s bar:gender ?gender .
+?gender rdfs:label ?gen . }
+?s bar:token ?token .
+}
+}
+}
+ORDER BY ?lex DESC(?num) ?pers DESC(?gen)")
 
 
+;;generate standard SPARQL query using stencil/render-string. 
+;;In this version the "for [value values]" section does not
+;;stiplulate that the property to which the value belongs
+;;have an rdfs:label. See sparql/pdgmqry-sparql-alt for
+;;older version.
+(defn pdgmqry-sparql [language lpref valstring]
+    (let [values (split valstring #",")
+          Language (capitalize language)
+          ]
+      (str
+      (tmpl/render-string 
+       (str
+	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+	PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+	PREFIX aama: <http://id.oi.uchicago.edu/aama/2013/> 
+	PREFIX aamas: <http://id.oi.uchicago.edu/aama/2013/schema/> 
+	PREFIX aamag:	 <http://oi.uchicago.edu/aama/2013/graph/> 
+	PREFIX {{lpref}}:   <http://id.oi.uchicago.edu/aama/2013/{{language}}/> 
+	SELECT ?lex ?num ?pers ?gen ?token  
+	WHERE\n{ 
+	 { 
+	  GRAPH aamag:{{language}}\n  { 
+	   ?s {{lpref}}:pos {{lpref}}:Verb .  
+	   ?s aamas:lang aama:{{Language}} . 
+	   ?s aamas:lang ?lang . 
+	   ?lang rdfs:label ?langLabel .  ")
+       {:lpref lpref
+        :language language
+        :Language Language})
+      (apply str  
+             (for [value values]
+        (tmpl/render-string 
+         (str
+          "?s ?Q{{value}}  {{lpref}}:{{value}} .  ")
+         {:value value
+          :lpref lpref})
+          )
+      )
+      (tmpl/render-string
+       (str
+       "   OPTIONAL { ?s aamas:lexeme ?lex . }  
+	   OPTIONAL { ?s {{lpref}}:number ?number .  
+	   ?number rdfs:label ?num . } 
+	   {   ?s {{lpref}}:pngShapeClass ?person .}  
+	   UNION  
+	   {   ?s {{lpref}}:person ?person .}  
+	   ?person rdfs:label ?pers .  
+	   OPTIONAL { ?s {{lpref}}:gender ?gender .  
+	   ?gender rdfs:label ?gen . } 
+	   ?s {{lpref}}:token ?token .  
+	  } 
+	 } 
+	} 
+	ORDER BY ?lex DESC(?num) ?pers DESC(?gen) ")
+       {:lpref lpref})
+       );;str
+))
 
-(defn home []
+
+(defn home 
+  []
   (layout/common [:h1 "PDGM Display"]
                  [:p "Welcome to PDGM Display"]
                  ;; [:p error]
                  [:hr]
                  (form-to [:post "/pdgm"]
-                          [:p "Language:" (text-field "language")]
-                          [:p "LangAbbrev:" (text-field "lpref")]
-                          [:p "Value String:" (text-field "valstring")]
+                          [:p "Language:" (text-field "language" "beja-arteiga")]
+                          [:p "LangAbbrev:" (text-field "lpref" "bar")]
+                          [:p "Value String:" (text-field "valstring" "Prefix,Affirmative,CCY,Aorist")]
                            (submit-button "Get pdgm"))
                           [:hr]))
 
 (defroutes home-routes
   (GET "/" [] (home))
-  (POST "/pdgm" [language lpref ] 
-        ;;(layout/common
-         ;;(show-pdgm language labbrev valstring))))
-        (aama-qry)
-        (pdgm-qry language lpref)
+  (POST "/pdgm" [language lpref valstring] 
+        (layout/common
+        (def query-sparql (pdgmqry-sparql language lpref valstring))
+        (def query-matsu (pdgmqry-matsu language lpref valstring))
+        [:p "sparql = " query-sparql]
+        [:p "matsu = " query-matsu])
         )
-  (GET "/sparql"
-       [language lpref]
+  (GET "/sparql" []
+       ;;[language lpref valstring]
        ;; send SPARQL over HTTP request
        (let [req (http/get aama
                            {:query-params
-                            {"query" (pdgm-qry language lpref)
+                            ;;{"query" (aama-qry) ;;canned matsu
+                            ;;{"query" query-matsu ;;generated matsu
+                            ;;{"query" pdgm-qry ;;canned sparql
+                            {"query" query-sparql ;;generated sparql
                              ;;"format" "application/sparql-results+json"}})]
                              "format" "text"}})]
          (log/info "sparql result status: " (:status req))
          (layout/common
           [:body
-           [:h1 "Result"]
-           [:pre (:body req)]]))))
+           [:h1 "Result with query-sparql"]
+           [:pre (:body req)]
+           [:p "sparql = " query-sparql]]))))
+ 
