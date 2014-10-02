b1d5fb7f37fe77d7c707adf3e1690f0051b6544f
Gene Gragg <g-gragg@uchicago.edu>
2014-10-02 12:08:32 -0500

    added cljs file

---
+++ src/cljs/webapp/core.cljs
@@ -0,0 +1,43 @@
+(ns webapp.core)
+
+;; (enable-console-print!)
+
+;; (println "Hello howdy world!")
+
+(defn handle-click []
+  (js/alert "Hello there!"))
+
+(def clickable (.getElementById js/document "clickable"))
+
+(.addEventListener clickable "click" handle-click)
+src/cljs/webapp/core.cljs (END)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
