367a81e15c287497a5b412b576c8593a797a658f
Gene Gragg <g-gragg@uchicago.edu>
2016-08-05 14:25:55 -0500

    README edits

--- bin/aama-cpwebapp2lngrepo.sh
+++ bin/aama-cpwebapp2lngrepo.sh
@@ -7,12 +7,18 @@ echo "webapp ********************************************"
 echo "copying ~/webapp  to aama/webapp"
 
 cd ~/webapp
+# lein ring uberjar
+cp target/webapp-0.1.0-SNAPSHOT-standalone.jar aama-webapp.jar
 cp LICENSE ../aama/webapp/
 cp README.md ../aama/webapp/
 cp project.clj ../aama/webapp/
 cp aama-webapp.jar ../aama/webapp/
+cp bin/aama-datastore-update.sh ../aama/webapp/bin/
+cp bin/aama-edn2rdf.sh ../aama/webapp/bin/
+cp bin/aama-rdf2fuseki.sh ../aama/webapp/bin/
+cp bin/*.rq ../aama/webapp/bin/
+cp bin/fu*.sh ../aama/webapp/bin/
 cp -R etc/* ../aama/webapp/etc/
-cp -R bin/* ../aama/webapp/bin/
 cp -R src/* ../aama/webapp/src/
 cp -R resources/* ../aama/webapp/resources/
 cd ../aama/webapp
